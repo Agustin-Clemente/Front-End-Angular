1934a9f9477c1911d579cf777a67c161ada3eb49
Agush <a_clemente@live.com.ar>
2023-07-18 14:53:33 -0300

    Modal cargando

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,16 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    Swal.fire({
+      position: 'center',
+      icon: 'warning',
+      title: 'Cargando, por favor espere',
+      text: 'Esto puede demorar varios minutos',
+      color: 'black',
+      background: 'aqua',
+      showConfirmButton: true,
+      iconColor: 'black',
+      confirmButtonColor: 'black'
+    })
   }
-
 }
